6da3bafb5300a4a9e544cf7c216a54a034a2c7c0
shintaroDesu <snkraishin87@gmail.com>
2018-09-27 17:31:27 +0200

    revert to ae9e44b0b7d5f633c0ee650f8028260f8bdefe66

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,30 +8,6 @@ public class TicTacToe {
 		random = new Random();
 	}
 
-	private boolean cannotBlock(int player) {
-		boolean firstLost = false;
-		for (int x = 0; x < board.length; x++) {
-			for (int y = 0; y < board.length; y++) {
-
-				if(board[x][y] == 0) {
-					board[x][y] = player;
-
-					if (win() == player) {
-						if (firstLost) {
-							board[x][y] = 0;
-							return true;
-						}
-						else
-							firstLost = true;
-					}
-
-					board[x][y] = 0;
-				}
-			}
-		}
-		return false;
-	}
-
 	public boolean tick(int row, int column, int player) {
 
 		if(board[row][column] != 0)
@@ -41,8 +17,111 @@ public class TicTacToe {
 		return true;
 	}
 
+	/**
+	 *
+	 * @param player        who is supposed to win, either 1 or 2
+	 * @param turn          current turn
+	 * @param point
+	 * @param pointMin      most optimal point that can get
+	 * @param currentDepth  level of difficulty
+	 * @param depthMax
+	 * @return array coord  int[0] row, int[1] column, int[2] pointMin, int[3] draw : 0, win : 1, lose : -1
+	 */
+	public int[] choose(int player, int turn, int point, int pointMin, int currentDepth, int depthMax) {
+		if(currentDepth == depthMax)
+			return new int[]{-1, -1, -1, 0}; // stop the method without any result
+
+		if(point > pointMin && pointMin != -1)
+			return new int[]{-1, -1, -1, 0}; // stop the method without any result
+
+		int[] coord = {-1, -1, -1, 0}; // coord[2] is pointMin
+		int[] result = {-1, -1, -1, 0};
+
+		/*
+		 * firstLost set to true when we can lose
+		 * and if firstLost is true and we lose in another coord
+		 * then there's no way to block it
+		 * choose another move
+		 */
+		boolean firstLost = false;
+		for(int x = 0; x < board.length; x++) {
+			for(int y = 0; y < board.length; y++) {
+				if(board[x][y] == 0) {
+
+					board[x][y] = turn;
+
+					// initialize coord if it's uninitialized or the previous move is bad
+
+					if(coord[0] == -1 || coord[3] == -1 || coord[3] == -2) {
+						coord[0] = x;
+						coord[1] = y;
+						coord[2] = pointMin;
+						coord[3] = 0;
+					}
+
+					int winner = win();
+					if(winner == player) {
+						board[x][y] = 0;
+						return new int[]{x, y, point, 1};
+					} else if(winner != 0) {
+						board[x][y] = 0;
+						/*return new int[]{-1, -1, point, -1};*/
+						if(!firstLost)
+							firstLost = true;
+						else
+							return new int[]{-1, -1, point, -2};
+					}
+
+					// change turn
+					int tempTurn;
+					if(turn == 1)
+						tempTurn = 2;
+					else
+						tempTurn = 1;
+
+					/*
+					 * when firstLost is true, we will only focus on finding another possibility that we can lose
+					 */
+
+					if(!firstLost) {
+						// min max here
+						result = choose(player, tempTurn, point + 1, pointMin, currentDepth + 1, depthMax);
+						if (coord[0] == x && coord[1] == y)
+							coord[3] = result[3]; // update status win or lose for the current coord
+
+
+						// if it's impossible to block which means result[3] == -2, and the turn is not player, return
+						if(result[3] == -2 && turn != player) {
+							board[x][y] = 0;
+							return new int[]{-1, -1, point, -1};
+						}
+
+						if (result[0] != -1) { // if it actually played
+							// random choice if the next move is not worse than the current move
+							boolean choice = result[2] == pointMin && random.nextInt(2) == 1 && result[3] == 1;
+
+							if ((result[2] < pointMin && result[3] == 1) || pointMin == -1 || choice) { // check if it's a better solution
+								pointMin = result[2];
+								coord[0] = x;
+								coord[1] = y;
+								coord[2] = result[2];
+								coord[3] = result[3];
+							}
+
+
+						}
+					}
+					board[x][y] = 0;
+				}
+			}
+		}
+		if(firstLost)
+			return new int[]{-1, -1, point, -1};
+		return coord;
+	}
+
 	public int win() {
-		// check column and row
+		 // check column and row
 		for(int x = 0; x < board.length; x++) {
 			if(board[0][x] == board[1][x] && board[0][x] == board[2][x] && board[0][x] != 0)
 				return board[0][x];
@@ -50,7 +129,7 @@ public class TicTacToe {
 				return board[x][0];
 		}
 
-		// check diagonal
+		 // check diagonal
 		if(board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != 0)
 			return board[0][0];
 
@@ -76,108 +155,4 @@ public class TicTacToe {
 			System.out.print("\n");
 		}
 	}
-	/**
-	 *
-	 * @param player        who is supposed to win, either 1 or 2
-	 * @param turn          current turn
-	 * @param point
-	 * @param pointMin      most optimal point that can get
-	 * @param currentDepth  level of difficulty
-	 * @param depthMax
-	 * @return array coord  int[0] row, int[1] column, int[2] pointMin, int[3] draw : 0, win : 1, lose : -1
-	 */
-	public int[] choose(int player, int turn, int point, int pointMin, int currentDepth, int depthMax) {
-		if(currentDepth == depthMax)
-			return new int[]{-1, -1, -1, 0}; // stop the method without any result
-
-		if(point > pointMin && pointMin != -1)
-			return new int[]{-1, -1, -1, 0}; // stop the method without any result
-
-		int[] coord = {-1, -1, -1, 0}; // coord[2] is pointMin
-
-		/*
-		 * firstLost set to true when we can lose
-		 * and if firstLost is true and we lose in another coord
-		 * then there's no way to block it
-		 * choose another move
-		 */
-
-		for(int x = 0; x < board.length; x++) {
-			for(int y = 0; y < board.length; y++) {
-				if(board[x][y] == 0) {
-
-					board[x][y] = turn;
-
-					// initialize coord if it's uninitialized or the previous move is bad
-
-					if(coord[0] == -1 || coord[3] == -1 || coord[3] == -2) {
-						coord[0] = x;
-						coord[1] = y;
-						coord[2] = pointMin;
-
-						if(coord[3] == -2)
-							coord[3] = 2; // more priority than 1
-						else
-							coord[3] = 0;
-					}
-
-					if (cannotBlock(player)) {
-						board[x][y] = 0;
-						return new int[]{-1, -1, point, -2};
-					}
-
-					int winner = win();
-					if(winner == player) {
-						board[x][y] = 0;
-						return new int[]{x, y, point, 1};
-					} else if(winner != 0) {
-						board[x][y] = 0;
-
-						return new int[]{-1, -1, point, -1};
-					}
-
-
-					// change turn
-					int tempTurn;
-					if(turn == 1)
-						tempTurn = 2;
-					else
-						tempTurn = 1;
-
-
-					// min max here
-					int[] result = choose(player, tempTurn, point + 1, pointMin, currentDepth + 1, depthMax);
-					if (coord[0] == x && coord[1] == y)
-						coord[3] = result[3]; // update status win or lose for the current coord
-
-
-					// if it's impossible to block which means result[3] == -2, and the turn is not player, return
-					if(result[3] == -2 && turn != player) {
-						board[x][y] = 0;
-						return new int[]{-1, -1, point, -2};
-					}
-
-					// if it actually played and there's no impossible toblock move
-					if (result[0] != -1 && coord[3] != 2) {
-						// random choice if the next move is not worse than the current move
-						boolean choice = result[2] == pointMin && random.nextInt(2) == 1 && result[3] == 1;
-
-						if ((result[2] < pointMin && result[3] == 1) || pointMin == -1 || choice) { // check if it's a better solution
-							pointMin = result[2];
-							coord[0] = x;
-							coord[1] = y;
-							coord[2] = result[2];
-							coord[3] = result[3];
-						}
-
-
-					}
-					board[x][y] = 0;
-				}
-			}
-		}
-		return coord;
-	}
-
-
 }
